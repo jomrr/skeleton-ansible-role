e18e4be46cde5278d78604f2bb08912174d00e02
jam82 <jam@kabelmail.net>
2020-04-20 02:42:56 +0200

    vars file for converge, pytest access to play vars

--- molecule/resources/tests/test_default.py
+++ molecule/resources/tests/test_default.py
@@ -1,9 +1,36 @@
 import os
 
+import pytest
+
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+@pytest.fixture
+def get_vars(host):
+    ansible_facts = host.ansible("setup")
+    ansible_distribution = ansible_facts["ansible_facts"]["ansible_distribution"]
+
+    defaults_files = "file=../../defaults/main.yml"
+    playbook_vars = "file=../resources/playbooks/vars.yml"
+    vars_files = "file=../../vars/" + ansible_distribution + ".yml"
+
+    ansible_vars = host.ansible(
+        "include_vars", defaults_files)["ansible_facts"]
+
+    ansible_vars.update(host.ansible(
+        "include_vars", playbook_vars)["ansible_facts"])
+
+    ansible_vars.update(host.ansible(
+        "include_vars", vars_files)["ansible_facts"])
+
+    return ansible_vars
+
+
+def test_common(host, get_vars):
+    assert get_vars['common_role_enabled'] is True
 
 
 def test_system_info(host):
